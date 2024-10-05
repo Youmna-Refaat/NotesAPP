3de0de2b4fec47e4356233666eeeae757396e1f4
YoumnaRefaat <youmnarefaat884@gmail.com>
2024-10-05 21:33:31 +0300

    create add note cubit and states

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'add_note_states.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
+  Color color = Colors.blueGrey;
+
+  addNote(NoteModel note) async {
+    note.color = color.value;
+    emit(AddNoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNotefailure(e.toString()));
+    }
+  }
+}
