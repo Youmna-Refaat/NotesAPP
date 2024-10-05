f7791c213de59e971073b48262de9731bcd1f33b
YoumnaRefaat <youmnarefaat884@gmail.com>
2024-10-05 21:29:18 +0300

    constants file

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 138, 166, 189);
+const kNotesBox = 'notes_box';
+List<Color> kColors = const [
+  Colors.blueGrey,
+  Color.fromARGB(255, 80, 171, 196),
+  Color.fromARGB(243, 34, 137, 166),
+  Color.fromARGB(255, 52, 80, 149),
+  Color.fromARGB(255, 12, 87, 112),
+  Color.fromARGB(255, 62, 91, 144),
+];
