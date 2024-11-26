8be5c987f7564efe3adc99f010fcef8d0849c1cb
Youmna-Refaat <youmnarefaat884@gmail.com>
2024-11-26 21:11:39 +0200

    update in app files

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ void main() async {
   Bloc.observer = SimpleBlocObserver();
   Hive.registerAdapter(NoteModelAdapter());
   await Hive.openBox<NoteModel>(kNotesBox);
-
   runApp(const NotesAPP());
 }
 
